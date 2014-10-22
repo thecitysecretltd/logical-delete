0252f570a40c6ce9a68246100838e4cae80d6346
Poundex <mail@poundex.net>
2014-10-22 21:43:55 +0100

    Update Grails version and BuildConfig

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -1,29 +1,41 @@
-grails.project.work.dir = 'target'
+grails.project.class.dir = "target/classes"
+grails.project.test.class.dir = "target/test-classes"
+grails.project.test.reports.dir = "target/test-reports"
 
+grails.project.fork = false
+
+grails.project.dependency.resolver = "maven" // or ivy
 grails.project.dependency.resolution = {
-
-	inherits 'global'
-	log 'warn'
-
+	// inherit Grails' default dependencies
+	inherits("global") {
+		// uncomment to disable ehcache
+		// excludes 'ehcache'
+	}
+	log "warn" // log level of Ivy resolver, either 'error', 'warn', 'info', 'debug' or 'verbose'
 	repositories {
 		grailsCentral()
 		mavenLocal()
 		mavenCentral()
-	}
 
+		//mavenRepo "http://repository.codehaus.org"
+		//mavenRepo "http://download.java.net/maven/2/"
+		//mavenRepo "http://repository.jboss.com/maven2/"
+	}
 	dependencies {
+
 	}
 
 	plugins {
-		build ':release:2.2.1', ':rest-client-builder:1.0.3', {
+		build(":release:3.0.1",
+				":rest-client-builder:1.0.3") {
 			export = false
 		}
-		compile(":hibernate:$grailsVersion") { export = false }
-		compile(':hawk-eventing:0.5.1') {
-			excludes 'svn'
-		}
-		compile(":hibernate-hijacker:0.8.1") {
-			excludes 'svn'
-		}
+
+		test ":code-coverage:2.0.3-2"
 	}
+}
+
+coverage {
+	enabledByDefault = true
+	xml = true
 }
